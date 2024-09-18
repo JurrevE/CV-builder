60f6b90f00d0118516f490b192456897a408c2e7
Jurre van Eijk <jurreveijk13@gmail.com>
2024-09-18 12:06:34 +0700

    fixed email naming lol

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 export default function Header(props) {
-  return (
-    <div className="header">
-      <h1 className="fullname">{props.name || "Jurre van Eijk"}</h1>
-      <div className="otherinfo">
-        <h3 className="otherinfoh3">{props.email || "yourmom@gmail.com"}</h3>
-        <h3 className="otherinfoh3">{props.phone || "06 16294209"}</h3>
-        <h3 className="otherinfoh3">
-          {props.address || "Maasstraat 123, 1823XG Alkmaar"}
-        </h3>
-      </div>
-    </div>
-  );
+	return (
+		<div className="header">
+			<h1 className="fullname">{props.name || "Jurre van Eijk"}</h1>
+			<div className="otherinfo">
+				<h3 className="otherinfoh3">
+					{props.email || "nicewebsite@gmail.com"}
+				</h3>
+				<h3 className="otherinfoh3">{props.phone || "06 16294209"}</h3>
+				<h3 className="otherinfoh3">
+					{props.address || "Maasstraat 123, 1823XG Alkmaar"}
+				</h3>
+			</div>
+		</div>
+	);
 }
